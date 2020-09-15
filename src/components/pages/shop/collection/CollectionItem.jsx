ba280f4cb2a99720190dd1a292b8983893f3d4cd
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-09-15 17:23:42 +0300

    Add navigation to product details page and prepare for deploy

--- src/components/pages/shop/collection/CollectionItem.jsx
+++ src/components/pages/shop/collection/CollectionItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addItem, openCart } from 'store/cart/actions';
 import { selectCollectionItem } from 'store/shop/selectors';
@@ -7,48 +8,59 @@ import { Button } from 'components/app/shared';
 
 const CollectionItem = ({ item, match, addItem, openCart }) => {
   return (
-    <div className='collectionItem-details'>
-      <div className='collectionItem-details-left'>
-        <div className='collectionItem-details-left__mainImage'>
-          <img src={item.imageUrl} alt='' />
+    <div>
+      <p className='collectionItem__navigation'>
+        <Link to='/shop'>
+          <span>Shop</span>
+        </Link>
+        <span> &#8594; </span>
+        <Link to={`/shop/${match.params.collectionId}`}>
+          <span>{match.params.collectionId}</span>
+        </Link>
+      </p>
+      <div className='collectionItem-details'>
+        <div className='collectionItem-details-left'>
+          <div className='collectionItem-details-left__mainImage'>
+            <img src={item.imageUrl} alt='' />
+          </div>
+          <div className='collectionItem-details-left__images'>
+            <img src={item.imageUrl} alt='' />
+            <img src={item.imageUrl} alt='' />
+            <img src={item.imageUrl} alt='' />
+          </div>
         </div>
-        <div className='collectionItem-details-left__images'>
-          <img src={item.imageUrl} alt='' />
-          <img src={item.imageUrl} alt='' />
-          <img src={item.imageUrl} alt='' />
+        <div className='collectionItem-details-right'>
+          <small>{match.params.collectionId.toUpperCase()}</small>
+          <h2>{item.name}</h2>
+          <StarRatingComponent
+            name='rate1'
+            starCount={5}
+            value={item.rating}
+            className='rating'
+          />
+          <div className='collectionItem-details-right__price'>
+            <p>${item.price} USD</p>
+          </div>
+          <div className='collectionItem-details-right__desc'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+              itaque quos ducimus, doloribus illo adipisci, beatae possimus
+              numquam delectus maxime praesentium neque libero. Non laboriosam
+              sint, iusto dolorum repellat molestiae dolor minus deleniti
+              asperiores illo. Doloremque eum, perspiciatis odio officia animi
+              nostrum quis ratione excepturi! Facilis facere velit deserunt
+              beatae iusto aliquid itaque saepe dolore?
+            </p>
+          </div>
+          <Button
+            type='button'
+            title='Add to Cart'
+            onClick={() => {
+              addItem(item);
+              openCart();
+            }}
+          />
         </div>
-      </div>
-      <div className='collectionItem-details-right'>
-        <small>{match.params.collectionId.toUpperCase()}</small>
-        <h2>{item.name}</h2>
-        <StarRatingComponent
-          name='rate1'
-          starCount={5}
-          value={item.rating}
-          className='rating'
-        />
-        <div className='collectionItem-details-right__price'>
-          <p>${item.price} USD</p>
-        </div>
-        <div className='collectionItem-details-right__desc'>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
-            itaque quos ducimus, doloribus illo adipisci, beatae possimus
-            numquam delectus maxime praesentium neque libero. Non laboriosam
-            sint, iusto dolorum repellat molestiae dolor minus deleniti
-            asperiores illo. Doloremque eum, perspiciatis odio officia animi
-            nostrum quis ratione excepturi! Facilis facere velit deserunt beatae
-            iusto aliquid itaque saepe dolore?
-          </p>
-        </div>
-        <Button
-          type='button'
-          title='Add to Cart'
-          onClick={() => {
-            addItem(item);
-            openCart();
-          }}
-        />
       </div>
     </div>
   );
