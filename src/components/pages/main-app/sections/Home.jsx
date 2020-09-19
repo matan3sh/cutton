2e6288d01a473f1f70959df0c853e9e8c7729af7
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-09-19 11:52:27 +0300

    Clean up unuse dependencies

--- src/components/pages/main-app/sections/Home.jsx
+++ src/components/pages/main-app/sections/Home.jsx
@@ -5,7 +5,7 @@ import home from 'assets/img/home.png';
 
 const Home = () => {
   return (
-    <section className='home' id='Home'>
+    <section className='home'>
       <div className='home__container bd-grid'>
         <div className='home__data'>
           <h1 className='home__title'>
